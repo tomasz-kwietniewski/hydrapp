5e3429de9ccc1b9b5b4d6bf4e3ab33f4adc1a161
tomasz-kwietniewski <64807094+tomasz-kwietniewski@users.noreply.github.com>
2020-08-11 22:00:40 +0200

    text format

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
 
-const glassCounter = document.querySelector('.glass__counter--js');
-const addButton = document.querySelector('.add-button--js');
-const removeButton = document.querySelector('.remove-button--js');
+const glassCounter = document.querySelector(".glass__counter--js");
+const addButton = document.querySelector(".add-button--js");
+const removeButton = document.querySelector(".remove-button--js");
 const key = new Date().toISOString().slice(0, 10);
 
 console.log(key);
@@ -20,23 +20,23 @@ let currentGlassCounter = 0;
 const localStorageValue = localStorage.getItem(key);
 
 if (localStorageValue) {
-    currentGlassCounter = localStorageValue;
+  currentGlassCounter = localStorageValue;
 } else {
-    localStorage.setItem(key, 0);
+  localStorage.setItem(key, 0);
 }
 
 glassCounter.innerHTML = currentGlassCounter;
 
-addButton.addEventListener('click', () => {
-    currentGlassCounter++;
-    glassCounter.innerHTML = currentGlassCounter;
-    localStorage.setItem(key, currentGlassCounter);
+addButton.addEventListener("click", () => {
+  currentGlassCounter++;
+  glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
 });
 
-removeButton.addEventListener('click', () => {
-    if (currentGlassCounter > 0) {
-        currentGlassCounter--;
-    }    
-    glassCounter.innerHTML = currentGlassCounter;
-    localStorage.setItem(key, currentGlassCounter);
+removeButton.addEventListener("click", () => {
+  if (currentGlassCounter > 0) {
+    currentGlassCounter--;
+  }
+  glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
 });
