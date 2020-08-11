795a854e0474e7ae195756dfc4330fc2216789e1
tomasz-kwietniewski <64807094+tomasz-kwietniewski@users.noreply.github.com>
2020-08-11 21:19:46 +0200

    addButton and removeButton are working correct and glassCounter cannot be under 0

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,21 @@ registerSW();
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const glassCounter = document.querySelector('.glass__counter--js');
+const addButton = document.querySelector('.add-button--js');
+const removeButton = document.querySelector('.remove-button--js');
+
+let currentGlassCounter = 0;
+
+addButton.addEventListener('click', () => {
+    currentGlassCounter++;
+    glassCounter.innerHTML = currentGlassCounter;
+});
+
+removeButton.addEventListener('click', () => {
+    if (currentGlassCounter > 0) {
+        currentGlassCounter--;
+    }    
+    glassCounter.innerHTML = currentGlassCounter;
+});
